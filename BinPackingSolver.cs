80732310c9bd1437f1756c88a27231510939f8b1
Vladislav Isaev <keyayeten@gmail.com>
2025-05-14 23:56:25 +0300

    console input solution

---
+++ BinPackingSolver.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace algo_coursework
+{
+    public class BinPackingSolver
+    {
+        public static int HeuristicBinPacking(int[] weights, int capacity)
+        {
+            Array.Sort(weights);
+            Array.Reverse(weights);
+
+            List<int> bins = new List<int>();
+
+            foreach (var weight in weights)
+            {
+                bool placed = false;
+                for (int i = 0; i < bins.Count; i++)
+                {
+                    if (bins[i] + weight <= capacity)
+                    {
+                        bins[i] += weight;
+                        placed = true;
+                        break;
+                    }
+                }
+
+                if (!placed)
+                {
+                    bins.Add(weight);
+                }
+            }
+
+            return bins.Count;
+        }
+
+        public static int BruteForceBinPacking(int[] weights, int capacity)
+        {
+            int n = weights.Length;
+            int minBins = n;
+
+            void TryBins(List<List<int>> bins, int index)
+            {
+                if (index == n)
+                {
+                    minBins = Math.Min(minBins, bins.Count);
+                    return;
+                }
+
+                var currentWeight = weights[index];
+
+                for (int i = 0; i < bins.Count; i++)
+                {
+                    if (bins[i].Sum() + currentWeight <= capacity)
+                    {
+                        bins[i].Add(currentWeight);
+                        TryBins(bins, index + 1);
+                        bins[i].RemoveAt(bins[i].Count - 1);
+                    }
+                }
+
+                bins.Add(new List<int> { currentWeight });
+                TryBins(bins, index + 1);
+                bins.RemoveAt(bins.Count - 1);
+            }
+
+            TryBins(new List<List<int>>(), 0);
+            return minBins;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace algo_coursework
 {
@@ -10,7 +7,54 @@ namespace algo_coursework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            UserInterface.Run();
+        }
+    }
+
+    public static class UserInterface
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Введите вместимость контейнера (b):");
+            if (!int.TryParse(Console.ReadLine(), out int b) || b <= 0)
+            {
+                Console.WriteLine("Ошибка: некорректное значение вместимости.");
+                return;
+            }
+
+            Console.WriteLine("Введите количество грузов (n):");
+            if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+            {
+                Console.WriteLine("Ошибка: некорректное количество грузов.");
+                return;
+            }
+
+            Console.WriteLine("Введите массы грузов (через пробел):");
+            int[] weights;
+
+            try
+            {
+                weights = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка: не удалось распарсить числа.");
+                return;
+            }
+
+            if (weights.Length != n)
+            {
+                Console.WriteLine("Ошибка: количество введённых грузов не совпадает с n.");
+                return;
+            }
+
+            Console.WriteLine("\n--- Эвристическое решение ---");
+            int heuristicResult = BinPackingSolver.HeuristicBinPacking(weights, b);
+            Console.WriteLine($"Минимальное число контейнеров (эвристика): {heuristicResult}");
+
+            Console.WriteLine("\n--- Полный перебор ---");
+            int bruteForceResult = BinPackingSolver.BruteForceBinPacking(weights, b);
+            Console.WriteLine($"Минимальное число контейнеров (перебор): {bruteForceResult}");
         }
     }
 }
